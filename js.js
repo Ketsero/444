aa9640bc08a4abd14f3ce12d3963f31f037d41ff
Ketsero <164652237+Ketsero@users.noreply.github.com>
2025-03-24 19:10:37 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+    let input = document.querySelector('.input');
+    if (input.value == "4shprog") {
+        input.style.border = "10px solid rgb(0, 255, 38)"
+        window.location.href = "Babaluda.html";
+    } else {
+        window.close.href = "golov2.html";
+    }
+};
+
